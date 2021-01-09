cd5f799dc621e75d78b84cde9fd50eba1e37e159
Artak Davtyan <artak22@yandex.ru>
2021-01-09 16:26:05 +0400

    Initial layou

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Layout.scss';
+
+const Layout = ({ children }) => {
+    return (
+        <div className='app-layout'>
+            {children}
+        </div>
+    )
+}
+
+export default Layout;
